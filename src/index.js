16290090d838335eb623ebb748eb43a654a6a484
Digrifine <digrifine@gmail.com>
2021-03-02 21:34:59 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    arr = n.toString().split("");
+    str = "";
+    arr = arr.reverse();
+    arr.forEach((element) => {
+        str = str + element;
+    });
+    num = Number(str);
+    return num;
+};
